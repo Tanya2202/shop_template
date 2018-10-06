dceca4c02e2fa1405e17e627a801f62de2a950c7
Tanya Lakhman <Tanya2202@ukr.net>
2018-10-06 18:42:46 +0300

    Add file 3

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,14 @@ $(function(){
 		$('.main-menu .navbar-form').slideToggle();
 	});
 });
+
+$(window).load(function(){
+	var carouselCaptionWidth = $("#carousel-sidebar .active img").width();
+   $("#carousel-sidebar img").each(function(){
+$(this).attr("width", carouselCaptionWidth);
+   });
+   $("#carousel-sidebar .sidebar-carousel-caption").css("max-width", carouselCaptionWidth +"px");
+$("#carousel-sidebar .carousel-indicators").css("max-width", carouselCaptionWidth +"px");
+$(".sidebar .banner").css("max-width", carouselCaptionWidth +"px");
+
+});
